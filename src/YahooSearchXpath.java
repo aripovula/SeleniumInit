962af16b0877441fa39fa071d092dc7f97de9854
Ulugbek Aripov <ulugbeks@gmail.com>
2019-06-21 01:43:43 -0400

    Added code using CSS and XPath

--- src/YahooSearchXpath.java
+++ src/YahooSearchXpath.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class YahooSearch {
+public class YahooSearchXpath {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,8 +15,12 @@ public class YahooSearch {
 		System.setProperty("webdriver.chrome.driver", "/Users/myfamily/Documents/00CurWorks/AllCode/Java/selenium-java-3/chromedriver");
 		WebDriver driver =  new ChromeDriver();
 		driver.get("http://yahoo.com");
-		driver.findElement(By.id("uh-search-box")).sendKeys("soap");
-		driver.findElement(By.id("uh-search-button")).click();
+		System.out.println(driver.findElement(By.xpath("//*[@id=\'uh-logo\']")).getText());
+		driver.findElement(By.xpath("//*[@id=\'uh-search-box\']")).sendKeys("soap");
+		driver.findElement(By.xpath("//*[@id=\'uh-search-button\']")).click();
+
+//		driver.findElement(By.linkText("More scores »")).click();
+		
 		driver.quit();
 	}
 
